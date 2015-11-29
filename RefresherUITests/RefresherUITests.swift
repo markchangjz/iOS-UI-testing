aa6d506cf001493d204e6a5933811ef59ae3cd6c
MarkChang <markchang@kkbox.com>
2015-11-29 17:25:21 +0800

    Add some test cases.

--- RefresherUITests/RefresherUITests.swift
+++ RefresherUITests/RefresherUITests.swift
@@ -73,72 +73,72 @@ class RefresherUITests: XCTestCase {
         XCTAssertEqual(originalNumberOfCell - 1, currentlyNumberOfCell)
     }
     
-//    func testDeleteItemBySwipeGesture() {
-//        let app = XCUIApplication()
-//        
-//        let originalNumberOfCell =  app.tables.staticTexts.count
-//        
-//        let firstItem = app.tables.staticTexts.elementBoundByIndex(0)
-//        firstItem.swipeLeft()
-//        
-//        XCUIApplication().tables.buttons["Delete"].tap()
-//        
-//        let currentlyNumberOfCell =  app.tables.staticTexts.count
-//        
-//        XCTAssertEqual(originalNumberOfCell - 1, currentlyNumberOfCell)
-//    }
-//    
-//    func testSearchItem() {
-//        let app = XCUIApplication()
-//        
-//        app.tables.searchFields["Search"].tap()
-//        app.tables.searchFields["Search"].typeText("japan")
-//        
-//        let searchFirstItem = app.tables.staticTexts.elementBoundByIndex(0)
-//        
-//        XCTAssertTrue(searchFirstItem.label.lowercaseString.containsString("japan"))
-//        
-//        let firstItem = app.tables.staticTexts.elementBoundByIndex(0)
-//        let firstItemName = firstItem.label
-//        
-//        firstItem.tap()
-//        let showItemString = app.staticTexts["detailLabel"].label
-//        
-//        XCTAssertEqual(firstItemName, showItemString)
-//    }
-//    
-//    func testPullDownToRefresh() {
-//        let app = XCUIApplication()
-//        
-//        addNewItem("A")
-//        addNewItem("zzzzz")
-//        
-//        pullDownToRefresh()
-//        let firstItem = app.tables.staticTexts.elementBoundByIndex(0)
-//        XCTAssertEqual(firstItem.label, "zzzzz")
-//        
-//        pullDownToRefresh()
-//        XCTAssertEqual(firstItem.label, "A")
-//    }
-//    
-//    func testReorder() {
-//        let app = XCUIApplication()
-//        
-//        app.navigationBars.buttons["Edit"].tap()
-//        let originalItem1Name = app.tables.staticTexts.elementBoundByIndex(0).label
-//        let originalItem2Name = app.tables.staticTexts.elementBoundByIndex(1).label
-//        
-//        let cell1 = app.tables.cells.elementBoundByIndex(0)
-//        let reorderButton1 = cell1.buttons.matchingPredicate(NSPredicate(format: "label BEGINSWITH 'Reorder'")).element
-//        let cell2 = app.tables.cells.elementBoundByIndex(1)
-//        let reorderButton2 = cell2.buttons.matchingPredicate(NSPredicate(format: "label BEGINSWITH 'Reorder'")).element
-//        reorderButton1.pressForDuration(0.5, thenDragToElement: reorderButton2)
-//        app.navigationBars.buttons["Done"].tap()
-//        
-//        let currentlyItem1Name = app.tables.staticTexts.elementBoundByIndex(0).label
-//        let currentlyItem2Name = app.tables.staticTexts.elementBoundByIndex(1).label
-//
-//        XCTAssertEqual(originalItem1Name, currentlyItem2Name)
-//        XCTAssertEqual(originalItem2Name, currentlyItem1Name)
-//    }
+    func testDeleteItemBySwipeGesture() {
+        let app = XCUIApplication()
+        
+        let originalNumberOfCell =  app.tables.staticTexts.count
+        
+        let firstItem = app.tables.staticTexts.elementBoundByIndex(0)
+        firstItem.swipeLeft()
+        
+        XCUIApplication().tables.buttons["Delete"].tap()
+        
+        let currentlyNumberOfCell =  app.tables.staticTexts.count
+        
+        XCTAssertEqual(originalNumberOfCell - 1, currentlyNumberOfCell)
+    }
+    
+    func testSearchItem() {
+        let app = XCUIApplication()
+        
+        app.tables.searchFields["Search"].tap()
+        app.tables.searchFields["Search"].typeText("japan")
+        
+        let searchFirstItem = app.tables.staticTexts.elementBoundByIndex(0)
+        
+        XCTAssertTrue(searchFirstItem.label.lowercaseString.containsString("japan"))
+        
+        let firstItem = app.tables.staticTexts.elementBoundByIndex(0)
+        let firstItemName = firstItem.label
+        
+        firstItem.tap()
+        let showItemString = app.staticTexts["detailLabel"].label
+        
+        XCTAssertEqual(firstItemName, showItemString)
+    }
+    
+    func testPullDownToRefresh() {
+        let app = XCUIApplication()
+        
+        addNewItem("A")
+        addNewItem("zzzzz")
+        
+        pullDownToRefresh()
+        let firstItem = app.tables.staticTexts.elementBoundByIndex(0)
+        XCTAssertEqual(firstItem.label, "zzzzz")
+        
+        pullDownToRefresh()
+        XCTAssertEqual(firstItem.label, "A")
+    }
+    
+    func testReorder() {
+        let app = XCUIApplication()
+        
+        app.navigationBars.buttons["Edit"].tap()
+        let originalItem1Name = app.tables.staticTexts.elementBoundByIndex(0).label
+        let originalItem2Name = app.tables.staticTexts.elementBoundByIndex(1).label
+        
+        let cell1 = app.tables.cells.elementBoundByIndex(0)
+        let reorderButton1 = cell1.buttons.matchingPredicate(NSPredicate(format: "label BEGINSWITH 'Reorder'")).element
+        let cell2 = app.tables.cells.elementBoundByIndex(1)
+        let reorderButton2 = cell2.buttons.matchingPredicate(NSPredicate(format: "label BEGINSWITH 'Reorder'")).element
+        reorderButton1.pressForDuration(0.5, thenDragToElement: reorderButton2)
+        app.navigationBars.buttons["Done"].tap()
+        
+        let currentlyItem1Name = app.tables.staticTexts.elementBoundByIndex(0).label
+        let currentlyItem2Name = app.tables.staticTexts.elementBoundByIndex(1).label
+
+        XCTAssertEqual(originalItem1Name, currentlyItem2Name)
+        XCTAssertEqual(originalItem2Name, currentlyItem1Name)
+    }
 }
